a27896d03faffce0d659a985d184913b2d2ba0ae
d-rafal <dymek4r@gmail.com>
2023-01-04 13:35:09 +0100

    Updates

---
+++ static/js/392.5a473377.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrates=self.webpackChunkrates||[]).push([[392],{82392:function(e,t,n){n.r(t);var r=n(80184),s={fontSize:"4rem"};t.default=function(){return(0,r.jsx)("p",{style:s,children:"404"})}}}]);
+//# sourceMappingURL=392.5a473377.chunk.js.map
